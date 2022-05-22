fabaec02832a271813813542d948ca812244c501
nikola madzharov <nmadzharov1912@gmail.com>
2022-05-22 22:41:38 +0300

    100/100

---
+++ P.01 Inheritance - Lab/05. Stack of Strings/StartUp .cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            StackOfStrings stackOfStrings = new StackOfStrings();
+            Console.WriteLine(stackOfStrings.IsEmpty());
+            List<string> strings = new List<string>() { "2","4","323"};
+            stackOfStrings.AddRange(strings);
+            
+            Console.WriteLine(stackOfStrings.Pop());
+            Console.WriteLine(stackOfStrings.IsEmpty());
+            Console.WriteLine(stackOfStrings.Pop());
+            Console.WriteLine(stackOfStrings.Pop());
+            Console.WriteLine(stackOfStrings.IsEmpty());
+
+        }
+    }
+}
