5fecf774d893f5814251c87b1669383d7832d648
nikola madzharov <nmadzharov1912@gmail.com>
2022-05-29 23:51:40 +0300

    100/100

---
+++ P.02 Inheritance - Exercise/Person/Child.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance___Exercise
+{
+    public class Child : Person
+    {
+        public Child(string name, int age) : base(name, age)
+        {
+
+        }
+    }
+}
