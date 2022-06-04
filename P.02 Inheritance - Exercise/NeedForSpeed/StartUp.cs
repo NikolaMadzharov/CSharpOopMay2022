748a819f16d84a8f034adcd0fb57f05298718718
koko1912 <nmadzharov1912@gmail.com>
2022-06-04 18:19:37 +0300

    100/100

---
+++ P.02 Inheritance - Exercise/NeedForSpeed/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NeedForSpeed 
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Vehicle sportCar = new SportCar(150, 100);
+            sportCar.Drive(8);
+            Console.WriteLine(sportCar.Fuel);
+            Console.WriteLine(sportCar.FuelConsumption);
+        }
+    }
+}
---
+++ P.02 Inheritance - Exercise/NeedForSpeed/Vehicle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public class Vehicle
+    {
+        public Vehicle( int hoursePower, double fuel)
+        {
+            Fuel = fuel;
+            HoursePower = hoursePower;
+        }
+
+        public virtual double FuelConsumption => 1.25;
+
+        public double Fuel { get; set; }
+        public int HoursePower { get; set; }
+
+
+
+        public virtual void Drive(double kilometers)
+        {
+            if (this.Fuel-(kilometers*FuelConsumption)>=0)
+            {
+                this.Fuel -= kilometers*FuelConsumption;
+            }
+        }
+    }
+}
