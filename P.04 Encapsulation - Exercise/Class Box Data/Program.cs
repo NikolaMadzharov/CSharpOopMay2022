c0460ee08eaa8a96aff036083c5293949f38a66a
nikola madzharov <nmadzharov1912@gmail.com>
2022-06-13 22:24:16 +0300

    100/100

---
+++ P.04 Encapsulation - Exercise/Class Box Data/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ClassBoxData
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            try
+            {
+                Console.WriteLine(new Box(length,height,width));
+            }
+            catch (ArgumentException e)
+            {
+
+                Console.WriteLine(e.Message);
+            }    
+        }
+    }
+}
