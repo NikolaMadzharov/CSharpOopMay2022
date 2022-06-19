5c14417140ba81d2a58ada7d85defd3e2e6cc46b
nikola madzharov <nmadzharov1912@gmail.com>
2022-06-20 00:00:13 +0300

    100/100

---
+++ P.06  Interfaces and Abstraction - Exercise/Define an Interface IPerson/Citizen .cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonInfo
+{
+    public class Citizen : IPerson
+    {
+        public Citizen(string name,int age)
+        {
+            Age = age;
+            Name = name;
+        }
+
+        public int Age { get; set; }
+        public string Name { get; set; }
+
+
+    }
+}
