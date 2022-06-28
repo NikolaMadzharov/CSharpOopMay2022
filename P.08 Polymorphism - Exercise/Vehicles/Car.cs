a5cc8684cf9f3252c1c0fdcc837fdaa8bec07912
nikola madzharov <nmadzharov1912@gmail.com>
2022-06-28 23:00:07 +0300

    100/100

---
+++ P.08 Polymorphism - Exercise/Vehicles/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vehicles
+{
+    public class Car : Vehicle
+    {
+        public Car(double fuelQuantity, double fuelConsumptionPerKm) : base(fuelQuantity, fuelConsumptionPerKm) 
+        { 
+        }
+       
+
+        public override double fuelConsumptionPerKm => base.fuelConsumptionPerKm + 0.90;
+    }
+}
