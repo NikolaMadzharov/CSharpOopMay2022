a00428882b8ffb4e8c9ee89b390544afcc895df6
nikola madzharov <nmadzharov1912@gmail.com>
2022-06-29 23:28:13 +0300

    100/100

---
+++ P.08 Polymorphism - Exercise/Vehicles/IVehicle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vehicles
+{
+    public interface IVehicle
+    {
+        public double fuelQuantity { get; set; }
+
+        public  double fuelConsumptionPerKm { get; set; }
+
+
+        public void Drive(double km);
+
+        public void Refuel(double km);
+
+        public bool IsItEnoughFuel(double km);
+
+    }
+}
