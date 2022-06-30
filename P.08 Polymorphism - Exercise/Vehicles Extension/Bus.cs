daf9b4cb5e6c511318d35e09f5fe3aade1a776c8
nikola madzharov <nmadzharov1912@gmail.com>
2022-06-30 21:49:46 +0300

    100/100

---
+++ P.08 Polymorphism - Exercise/Vehicles Extension/Bus.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vehicles
+{
+    public class Bus : Vehicle
+
+    {
+        public Bus(double fuelQuantity, double fuelConsumptionPerKm, double tankCapacity) : base(fuelQuantity, fuelConsumptionPerKm, tankCapacity)
+        {
+        }
+
+        public override double fuelConsumptionPerKm =>this.IsItEmpty?base.fuelConsumptionPerKm : base.fuelConsumptionPerKm+1.4;
+	
+
+	
+    }
+}
