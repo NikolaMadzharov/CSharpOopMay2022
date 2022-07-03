440583203018e9f0d354481222d32b3e01f07987
nikola madzharov <nmadzharov1912@gmail.com>
2022-07-03 22:10:33 +0300

    100/100

---
+++ P.09 UnitTesting - Lab/Test Axe/AxeTests.cs
@@ -0,0 +1,27 @@
+using NUnit.Framework;
+
+namespace Skeleton.Tests
+{
+    [TestFixture]
+    public class AxeTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Axe axe = new Axe(10, 10);
+            Dummy dummy =  new Dummy(10,10);
+
+            axe.Attack(dummy);
+
+            Assert.That(axe.DurabilityPoints, Is.EqualTo(9));
+        }
+        [Test]
+        public void Test2()
+        {
+            Axe axe = new Axe(10, -10);
+            Dummy dummy = new Dummy(10, 10);
+
+            Assert.That(() => axe.Attack(dummy), Throws.InvalidOperationException);
+        }
+    }
+}
