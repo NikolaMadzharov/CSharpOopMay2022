6ed618764f5390e6a27a7a1c2560ba424cd63346
nikola madzharov <nmadzharov1912@gmail.com>
2022-08-14 21:43:09 +0300

    50/50

---
+++ P. 10 ExamPreparation/Exam 14.8.22/Structure/Models/MilitaryUnits/MilitaryUnit .cs
@@ -0,0 +1,44 @@
+﻿namespace PlanetWars.Models.MilitaryUnits
+{
+    using System;
+
+    using PlanetWars.Models.MilitaryUnits.Contracts;
+
+    public abstract class MilitaryUnit:IMilitaryUnit
+    {
+        private double cost;
+
+        private  int enduranceLevel;
+
+        public MilitaryUnit(double cost)
+        {
+            this.Cost=cost;
+            this.EnduranceLevel = 1;
+        }
+
+        public double Cost
+        {
+            get => this.cost;
+           private set => this.cost = value;
+        }
+
+        public int EnduranceLevel
+        {
+            get => this.enduranceLevel;
+           private set =>
+               this.enduranceLevel = value;
+               
+           
+        }
+
+        public void IncreaseEndurance()
+        {
+            this.EnduranceLevel += 1;
+            if (this.EnduranceLevel>=20)
+            {
+                this.EnduranceLevel = 20;
+                throw new ArgumentException("Endurance level cannot exceed 20 power points.");
+            }
+        }
+    }
+}
---
+++ P. 10 ExamPreparation/Exam 14.8.22/Structure/Repositories/PlanetRepository.cs
@@ -0,0 +1,40 @@
+﻿
+
+
+namespace PlanetWars.Repositories
+{
+    using PlanetWars.Models.Planets.Contracts;
+    using PlanetWars.Repositories.Contracts;
+
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class PlanetRepository : IRepository<IPlanet>
+    {
+        private List<IPlanet> models;
+        public PlanetRepository()
+        {
+            this.models = new List<IPlanet>();
+        }
+        public IReadOnlyCollection<IPlanet> Models => this.models.AsReadOnly();
+
+        public void AddItem(IPlanet model)
+        {
+            this.models.Add(model);
+        }
+
+        public IPlanet FindByName(string name)
+        {
+            return this.models.FirstOrDefault(x => x.Name == name);
+        }
+
+        public bool RemoveItem(string name)
+        {
+            return models.Remove(models.FirstOrDefault(p => p.Name == name));
+
+
+
+
+        }
+    }
+}
